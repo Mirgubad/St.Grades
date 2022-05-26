caf0f672930e4b31ae4104cdedbe32ed63786038
Mirgubad <migubad@code.edu.az>
2022-05-26 17:47:59 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            {
+                st.Grade = new int[] {1,2,3,4,5,6,7,8 };
+            }
+        }
+    }
+}
---
+++ ConsoleApp41/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp41
+{
+    class Student
+    {
+        private int[] _grade;
+        public int[] Grade
+        {
+            get
+            {
+                return _grade;
+            }
+            set
+            {
+                int counter = 0;
+                int[] _grade = new int[value.Length - counter];
+                for (int i = 0, j = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        _grade[j] = value[i];
+                        j++;
+                        Console.WriteLine(_grade[j - 1]);
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+   }
+}
